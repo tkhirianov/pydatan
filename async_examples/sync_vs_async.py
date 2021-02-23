c563dc268be42ae296341246796ecc7f67cc6631
kunansy <kolobov.kirill@list.ru>
2021-02-23 18:12:21 +0300

    prototype implemented

--- async_examples/sync_vs_async.py
+++ async_examples/sync_vs_async.py
@@ -1,4 +1,33 @@
 #!/usr/bin/env python3
+import aiohttp
+import aiofiles
+import asyncio
+
+from pathlib import Path
+
+
+URLS_PATH = Path('urls.txt')
+
+
+async def fetch(session: aiohttp.ClientSession,
+                url: str,
+                **params) -> str:
+    pass
+        
+
+async def dump(data: str,
+               out_file: str) -> None:
+    pass
+
+
+async def worker(worker_id: int) -> None:
+    pass
+
+
+async def bound_fetch(urls: List[str],
+                      out_paths: List[str]) -> None:
+    pass
+
 
 def main() -> None:
     pass
